b6dcfe1edf90fe3bf64222f2081979b28c2cf029
Colin Williams <lackita@gmail.com>
2025-09-11 09:07:18 -0400

    Export additional functions

--- src/colloquy.ts
+++ src/colloquy.ts
@@ -1,5 +1,6 @@
 export type { ChatBot } from "./chat_bot.js";
 export { EchoBot } from "./echo_bot.js";
 export { OpenAIBot } from "./openai_bot.js";
+export { OpenAIMessageFactory } from "./openai/message.js"
 export { ClaudeBot } from "./claude_bot.js";
-export { PromptFunction, UnnamedFunctionError } from "./function.js";
+export { PromptFunction, PromptFunctionRepository, UnnamedFunctionError } from "./function.js";
